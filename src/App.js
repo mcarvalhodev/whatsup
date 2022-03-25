8f3b3eac82f3d5112958fe8dc16e5c7895f53a05
Marcelo Carvalho Santos] <mcarvalho.dev@gmail.com>
2022-03-25 14:24:50 -0300

    Initial project structure for whatslab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  state = {
+    messages: [],
+
+    valorInputUsuario: "",
+    valorInputMensagem: "",
+  };
+
+  sendMessage = () => {
+    const message = {
+      usuario: this.state.valorInputUsuario,
+      mensagem: this.state.valorInputMensagem,
+    };
+
+    const messages = [...this.state.messages, message];
+    this.setState({ messages: messages });
+
+    this.clearInputs();
+  };
+
+  clearInputs = () => {
+    this.setState({ valorInputUsuario: "" });
+    this.setState({ valorInputMensagem: "" });
+  };
+
+  onChangeInputUsuario = (event) => {
+    this.setState({ valorInputUsuario: event.target.value });
+  };
+
+  onChangeInputMensagem = (event) => {
+    this.setState({ valorInputMensagem: event.target.value });
+  };
+
+  render() {
+    const messages = this.state.messages.map((msg) => {
+      return (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>{msg.usuario}</span>: {msg.mensagem}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      );
+    });
+
+    return (
+      <div className="root">
+        <div className="container">
+          <div className="chat-container">{messages}</div>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Usuário"
+              className="user"
+              value={this.state.valorInputMensagem}
+              onChange={this.onChangeInputMensagem}
+            ></input>
+            <input
+              type="text"
+              placeholder="Mensagem"
+              className="message"
+              value={this.state.valorInputUsuario}
+              onChange={this.onChangeInputUsuario}
+            ></input>
+            <button onClick={this.sendMessage}>Enviar</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
