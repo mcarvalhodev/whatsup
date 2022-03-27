51960bc390816683135b89b59922fa665f768d1e
Marcelo Carvalho Santos] <mcarvalho.dev@gmail.com>
2022-03-27 13:08:29 -0300

    implementing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import "./App.css";
 import React from "react";
+import Message from "./components/Message/Message";
+
+import styled from "styled-components";
+
+const SendButton = styled.button`
+  border: none;
+  border-radius: 5px;
+  font-size: 1em;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const Usuario = styled.input`
+  width: 100px;
+  padding: 5px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Mensagem = styled.input`
+  flex: 1 1 0%;
+  padding: 8px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+`;
 
 class App extends React.Component {
   state = {
@@ -21,6 +48,13 @@ class App extends React.Component {
     this.clearInputs();
   };
 
+  onEnterSendMessage = (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      this.sendMessage();
+    }
+  };
+
   clearInputs = () => {
     this.setState({ valorInputUsuario: "" });
     this.setState({ valorInputMensagem: "" });
@@ -36,34 +70,29 @@ class App extends React.Component {
 
   render() {
     const messages = this.state.messages.map((msg) => {
-      return (
-        <p>
-          <span>{msg.usuario}</span>: {msg.mensagem}
-        </p>
-      );
+      return <Message nome={msg.usuario} conteudo={msg.mensagem}></Message>;
     });
 
     return (
-      <div className="root">
-        <div className="container">
+      <div className="container">
           <div className="chat-container">{messages}</div>
-          <div className="input-container">
-            <input
-              type="text"
-              placeholder="Usuário"
-              className="user"
-              value={this.state.valorInputMensagem}
-              onChange={this.onChangeInputMensagem}
-            ></input>
-            <input
-              type="text"
-              placeholder="Mensagem"
-              className="message"
-              value={this.state.valorInputUsuario}
-              onChange={this.onChangeInputUsuario}
-            ></input>
-            <button onClick={this.sendMessage}>Enviar</button>
-          </div>
+        <div className="input-container">
+          <Usuario
+            type="text"
+            placeholder="Usuário"
+            className="user"
+            value={this.state.valorInputUsuario}
+            onChange={this.onChangeInputUsuario}
+          />
+          <Mensagem
+            type="text"
+            placeholder="Mensagem"
+            className="message"
+            value={this.state.valorInputMensagem}
+            onChange={this.onChangeInputMensagem}
+            onKeyUp={this.onEnterSendMessage}
+          />
+          <SendButton onClick={this.sendMessage}>Send</SendButton>
         </div>
       </div>
     );
---
+++ src/components/Message/Message.js
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+
+import doublecheck from "../../images/doublecheck.svg";
+
+const BalaoMensagem = styled.div`
+  max-width: 60%;
+  word-wrap: break-word;
+  padding: 0.3em 0.8em;
+  border-radius: 0.5em;
+  margin: 0.2em 0;
+  line-height: 1.3;
+  font-weight: 450;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
+  position: relative;
+  background-color: ${(props) => {
+    if (props.tipo === "eu") {
+      return "#D9FDD3";
+    } else {
+      return "#ffffff";
+    }
+  }};
+
+  align-self: ${(props) => {
+    return props.tipo === "eu" ? "flex-end" : "flex-start";
+  }};
+
+  &:after {
+    content: "";
+    border: 15px solid transparent;
+    border-top-color: black;
+    position: absolute;
+    top: 0px;
+
+    border-top-color: ${(props) => {
+      return props.tipo === "eu" ? "#D9FDD3" : "#fff";
+    }};
+
+    right: ${(props) => {
+      if (props.tipo === "eu") {
+        return "-8px";
+      }
+    }};
+    left: ${(props) => {
+      if (props.tipo === "outro") {
+        return "-8px";
+      }
+    }};
+  }
+`;
+
+const Nome = styled.div`
+  color: #9aac8c;
+  font-size: 0.8em;
+  font-weight: 600;
+  margin-bottom: 0.2em;
+`;
+
+const DoubleCheck = styled.img`
+  position: absolute;
+  right: 4px;
+  height: 0.5em;
+  bottom: 4px;
+`;
+
+class Message extends React.Component {
+  state = {};
+
+  render() {
+    const nome = this.props.nome.toLowerCase();
+    if (nome === "eu") {
+      return (
+        <BalaoMensagem tipo={"eu"}>
+          {this.props.conteudo}
+          <DoubleCheck src={doublecheck} />
+        </BalaoMensagem>
+      );
+    } else {
+      return (
+        <BalaoMensagem tipo={"outro"}>
+          <Nome>{this.props.nome}</Nome>
+          <div>{this.props.conteudo}</div>
+        </BalaoMensagem>
+      );
+    }
+  }
+}
+
+export default Message;
